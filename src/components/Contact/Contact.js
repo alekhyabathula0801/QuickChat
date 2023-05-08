93711a4cdab51cf3a25ba549636df0e811995f33
Alekhya Bathula <alekhya@fisdom.com>
2023-05-08 17:02:23 +0530

    Added contact component

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,27 @@
+import React, { memo } from "react";
+import PropTypes from "prop-types";
+import "./Contact.scss";
+
+const Contact = (props) => {
+  const { title, icon, className, number, isActive } = props;
+  const activeCss = isActive ? "qc-c-active" : "";
+  return (
+    <div className={`qc-contact ${activeCss} ${className}`}>
+      <div className="qc-c-content">
+        <img alt="user" className="qc-c-image" src={icon} />
+        <div className={`qc-c-title ${activeCss}`}>{title}</div>
+      </div>
+      {number && <div className="qc-c-badge">{number}</div>}
+    </div>
+  );
+};
+
+export default memo(Contact);
+
+Contact.propTypes = {
+  isActive: PropTypes.bool,
+  icon: PropTypes.string,
+  title: PropTypes.string,
+  className: PropTypes.string,
+  number: PropTypes.number,
+};
