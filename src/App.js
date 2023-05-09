1596aed2bab92ad682ea62df1504a05158cca087
Alekhya Bathula <alekhya@fisdom.com>
2023-05-09 22:04:59 +0530

    WIP: Dashboard layout

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import React from "react";
 import { Provider } from "react-redux";
 import store from "./dataLayer/store";
+import Dashboard from "./pages/Dashboard";
 import "./App.css";
-import UserInfo from "./components/UserInfo";
-import ContactInfo from "./components/ContactInfo";
-import ShareLink from "./components/ShareLink";
-import WeekActivity from "./components/WeekActivity/WeekActivity";
-import FooterAction from "./components/InputBox";
-import ContactList from "./components/ContactList";
-import Conversations from "./components/Conversations";
 
 function App() {
   return (
     <Provider store={store}>
-      <Conversations />
-      <ContactList />
-      <UserInfo
-        title={"Bill Bradford"}
-        subtitle="Lead UX/UI Designer"
-        status={"Active"}
-        icon={
-          "https://www.goodmorningimagesdownload.com/wp-content/uploads/2021/12/Free-Sunflower-Unique-Whatsapp-DP-Images-Pics-Download-1.jpg"
-        }
-      />
-      <ContactInfo
-        shortName={"HB"}
-        name="Henry Boyd"
-        email={"henryboyd@gmail.com"}
-      />
-      <FooterAction />
-      <WeekActivity />
-      <ShareLink />
+      <Dashboard />
     </Provider>
   );
 }
---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import UserInfo from "../../components/UserInfo";
+import { USER_DATA } from "../../components/Conversations/constants";
+import ContactList from "../../components/ContactList";
+import Conversations from "../../components/Conversations";
+import ContactInfo from "../../components/ContactInfo";
+import WeekActivity from "../../components/WeekActivity/WeekActivity";
+import ShareLink from "../../components/ShareLink";
+import WrapperBox from "../../components/WrapperBox";
+import "./Dashboard.scss";
+
+const Dashboard = (props) => {
+  return (
+    <WrapperBox className="quick-chat-dashboard">
+      <div className="qcd-left-panel">
+        <UserInfo {...USER_DATA} />
+        <ContactList />
+      </div>
+      <div className="qcd-main-pannel">
+        <Conversations />
+      </div>
+      <div className="qcd-right-panel">
+        <ContactInfo
+          shortName={"HB"}
+          name="Henry Boyd"
+          email={"henryboyd@gmail.com"}
+        />
+        <WeekActivity />
+        <ShareLink />
+      </div>
+    </WrapperBox>
+  );
+};
+
+export default Dashboard;
+
+Dashboard.propTypes = {
+  number: PropTypes.number,
+};
