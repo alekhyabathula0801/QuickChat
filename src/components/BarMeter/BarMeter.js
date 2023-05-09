dfe0387f04e33ba576f8a8a80df7524b99e03944
Alekhya Bathula <alekhya@fisdom.com>
2023-05-09 15:21:00 +0530

    Added barmeter

---
+++ src/components/BarMeter/BarMeter.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./BarMeter.scss";
+
+const BarMeter = (props) => {
+  const { options = [], className = "" } = props;
+  return (
+    <div className={`qc-bar-meter ${className}`}>
+      {options.map((data, index) => {
+        return <Bar key={index} {...data} />;
+      })}
+    </div>
+  );
+};
+
+const Bar = ({ label = "", isActive = false, value }) => {
+  return (
+    <div className={`qc-bm-bar-wrapper ${isActive ? "active-bar" : ""}`}>
+      <div
+        className="qc-bm-bar"
+        style={{
+          height: `${value}%`,
+        }}
+      ></div>
+      <div className="qc-bm-dot" />
+      <div>{label}</div>
+    </div>
+  );
+};
+
+export default BarMeter;
+
+BarMeter.propTypes = {
+  options: PropTypes.array,
+  className: PropTypes.string,
+};
+
+Bar.propTypes = {
+  label: PropTypes.string,
+  isActive: PropTypes.bool,
+};
